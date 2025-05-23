22b52727450eaa7f94e67672ce65c9822bfc2738
Thales Monteiro Gerhardt <thalesmg3@gmail.com>
2025-05-23 17:49:46 -0300

    Delete repository directory

--- repository/MemoriaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.thalesmonteiro.backendfirebase.repository;
-
-
-import com.thalesmonteiro.backendfirebase.entity.Memoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemoriaRepository extends JpaRepository<Memoria, Long> {
-}
