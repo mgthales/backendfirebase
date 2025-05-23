fdfdae6ad930ea6ff69685dad3ad3ac5972c9cfd
Thales Monteiro Gerhardt <thalesmg3@gmail.com>
2025-05-23 15:32:19 -0300

    Add files via upload
    update api memorias

---
+++ src/main/java/com/thalesmonteiro/backendfirebase/repository/MemoriaRepository.java
@@ -0,0 +1,8 @@
+package com.thalesmonteiro.backendfirebase.repository;
+
+
+import com.thalesmonteiro.backendfirebase.entity.Memoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemoriaRepository extends JpaRepository<Memoria, Long> {
+}
