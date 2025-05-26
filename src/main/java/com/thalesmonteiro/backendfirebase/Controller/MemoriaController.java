4ba3a27a95569b7b5678a73bfb3b89392807e6ff
Thales Monteiro Gerhardt <thalesmg3@gmail.com>
2025-05-26 09:23:05 -0300

    Update MemoriaController.java

--- src/main/java/com/thalesmonteiro/backendfirebase/Controller/MemoriaController.java
+++ src/main/java/com/thalesmonteiro/backendfirebase/Controller/MemoriaController.java
@@ -4,6 +4,8 @@ import com.thalesmonteiro.backendfirebase.entity.Memoria;
 import com.thalesmonteiro.backendfirebase.repository.MemoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Sort;
+
 
 import java.util.List;
 
