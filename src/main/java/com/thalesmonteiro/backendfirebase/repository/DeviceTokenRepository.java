641aa392141f46696affbfb6f644741412b5918b
thales.gerhardt <thales.gerhardt@el.com.br>
2026-01-22 14:51:02 -0300

    acerto dos imports

--- src/main/java/com/thalesmonteiro/backendfirebase/repository/DeviceTokenRepository.java
+++ src/main/java/com/thalesmonteiro/backendfirebase/repository/DeviceTokenRepository.java
@@ -3,6 +3,8 @@ package com.thalesmonteiro.backendfirebase.repository;
 
 import com.thalesmonteiro.backendfirebase.entity.DeviceToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
